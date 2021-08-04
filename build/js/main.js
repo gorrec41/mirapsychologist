01c3793eef475f03c88bcdae022903d2f2a5e8e1
gorrec41 <batrak1@ya.ru>
2021-08-04 09:18:33 +0300

    add slider js

--- build/js/main.js
+++ build/js/main.js
@@ -1,1 +1,46 @@
-console.log('Hi!');
+window.addEventListener('DOMContentLoaded', () => {
+    const reviewsBlock = document.querySelectorAll('.reviews__item'),
+         arrowLeft = document.querySelector('.left'),
+         arrwRight = document.querySelector('.right');
+    let indexBlock = 0
+        
+    function hideVisibal(){
+        reviewsBlock.forEach((i)=>{
+            i.classList.remove('active')
+            i.classList.add('none')
+        })
+    }
+    function activeVisibal(g = 0){
+        hideVisibal()
+        reviewsBlock[g].classList.remove('none')
+        reviewsBlock[g].classList.add('active')
+    }
+    hideVisibal()
+    activeVisibal()
+    //----------slider------------//
+    arrowLeft.addEventListener('click', ()=>{
+        if(indexBlock<reviewsBlock.length-1){
+            indexBlock++
+            hideVisibal()
+            activeVisibal(indexBlock)
+        }else{
+            indexBlock=0
+            hideVisibal()
+            activeVisibal(indexBlock)
+            console.log('----')
+        }
+    })
+    arrwRight.addEventListener('click', ()=>{
+        if(indexBlock==0){
+            indexBlock=reviewsBlock.length-1
+            hideVisibal()
+            activeVisibal(indexBlock)
+            console.log(indexBlock)
+        }else{
+            indexBlock--
+            hideVisibal()
+            activeVisibal(indexBlock)
+        }
+    })
+    //-------//-slider-//---------//
+})
